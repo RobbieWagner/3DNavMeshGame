15e40670c6ccc941621ac70dc1ba87f327e61bd5
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-11-16 19:09:09 -0600

    Inchworm issues fixed!

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -11,22 +11,23 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField]
     private Transform playerPos;
 
-    private Vector3 returnPositon;
-
     [SerializeField]
     private Animator playerA;
-    [SerializeField]
-    private string inchingAnimation;
 
     private bool inching;
+    private bool inchingPart2;
 
     [SerializeField]
     private float displacement;
 
+    private Vector3 moveDir;
+
     void Start()
     {
-        playerA.enabled = false;
+        playerA.enabled = true;
         inching = false;
+        inchingPart2 = false;
+        moveDir = Vector3.forward;
     }
 
     //Update is called once per frame
@@ -34,7 +35,6 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(0f, 0f, vertical).normalized;
-        returnPositon = Vector3.zero;
 
         if (!inching && direction.magnitude >= 0.1f)
         {
@@ -43,45 +43,33 @@ public class ThirdPersonMovement : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            StartCoroutine(InchWorm(inchingAnimation, moveDir, displacement));
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Debug.Log("set direction " + moveDir.ToString());
+            StartCoroutine(InchWorm());
+        }
+        else if(inchingPart2) 
+        {
+            Debug.Log("get direction " + moveDir.ToString());
+            playerPos.Translate(moveDir.normalized * displacement * Time.deltaTime);
         }
     }
 
-    //  public IEnumerator InchWorm(string inchingAnimation, Vector3 moveDir, float displacement)
-    //  {
-    //     Debug.Log("Inching");
-
-    //     Debug.Log("Was Inching " + playerA.GetBool("Inching"));
-    //     playerA.SetBool("Inching", true);
-    //     Debug.Log("Is Inching" + playerA.GetBool("Inching"));
-
-    //     if(playerA.GetCurrentAnimatorStateInfo(0).IsName("InchwormInch")) Debug.Log("WTF");
-    //     Debug.Log(playerA.GetCurrentAnimatorStateInfo(0).length);
-    //     yield return new WaitForSeconds(playerA.GetCurrentAnimatorStateInfo(0).length);
-    //     playerA.SetBool("Inching", false);
-
-    //     Debug.Log("Inched");
-    //     //controller.Move(moveDir.normalized * displacement);
-        
-    //     inching = false;
-    //     StopCoroutine(InchWorm(inchingAnimation, moveDir, displacement));
-    //  }
-
-     public IEnumerator InchWorm(string inchingAnimation, Vector3 moveDir, float displacement)
+     public IEnumerator InchWorm()
      {
         Debug.Log("Inching");
-
-        playerA.enabled = true;
+        
+        playerA.SetBool("Inching", true);
         yield return new WaitForSeconds(playerA.GetCurrentAnimatorStateInfo(0).length);
-        playerA.enabled = false;
+        playerA.SetBool("Part2", true);
+        inchingPart2 = true;
+        yield return new WaitForSeconds(playerA.GetCurrentAnimatorStateInfo(0).length);
+        inchingPart2 = false;
+        playerA.SetBool("Part2", false);
+        playerA.SetBool("Inching", false);
 
         Debug.Log("Inched");
-        //controller.Move(moveDir.normalized * displacement);
-        playerPos.Translate(moveDir.normalized * displacement);
-        transform.position = returnPositon + playerPos.position;
         
         inching = false;
-        StopCoroutine(InchWorm(inchingAnimation, moveDir, displacement));
+        StopCoroutine(InchWorm());
      }
 }
